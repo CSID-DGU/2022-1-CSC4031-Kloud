841fef4e5999b472ffc8b2296f71a19ef6b29380
hanum <rntls123@naver.com>
2022-03-22 01:34:37 +0900

    recoil 로그인 상태관리 atom 설정

--- client/Router.tsx
+++ client/Router.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./route/Home";
 import Login from "./route/Login";
+import { useRecoilValue } from "recoil";
+import { isLoggedInAtom } from "./atoms";
 
 const Router = () => {
+  const isLoggedIn = useRecoilValue(isLoggedInAtom);
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact>
-          {/* 로그인 O -> 홈화면 
-            로그인 X -> 로그인화면 */}
           <Login></Login>
         </Route>
       </Switch>
