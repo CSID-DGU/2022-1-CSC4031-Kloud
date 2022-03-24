146c2fd371fc724425dbdfc67e8ad149be03c9d8
Seonghyeon Kim <kshshkim@gmail.com>
2022-03-24 13:59:37 +0900

    로그인, 클라이언트의 인프라 조회

--- server/app/client.py
+++ server/app/client.py
@@ -1,14 +1,37 @@
 import boto3
 
 
+identifiers = ['InternetGatewayId', 'VpcId', 'InternetGateways', 'LocalGateways', 'NetworkInterfaces']
+
+
 class KloudClient:
-    def __init__(self, access_key_id: str, session_instance: boto3.Session):
+    def __init__(self, access_key_id: str, session_instance: boto3.Session, ):
         self.id = access_key_id
-        self.session = session_instance
-        self.infra_info: dict
-        self.services: dict
+        self._session = session_instance
+        self._ec2_client = session_instance.client(service_name="ec2")
+        self._resources = dict()
+        self._describing_methods = {'VpcId': self._ec2_client.describe_vpcs,
+                                    # 'ZoneName': self._ec2_client.describe_availability_zones,
+                                    'SubnetId': self._ec2_client.describe_subnets,
+                                    'NetworkInterfaceId': self._ec2_client.describe_network_interfaces,
+                                    'InternetGatewayId': self._ec2_client.describe_internet_gateways,
+                                    'NatGatewayId': self._ec2_client.describe_nat_gateways}
 
-    def build_infra_dict(self):
-        pass
+    async def _update_resource_dict(self) -> None:
+        for identifier, describing_method in self._describing_methods.items():
+            response: dict = self.cut_useless_metadata(describing_method())
+            for dic in response:
+                primary_key = dic[identifier]
+                self._resources[primary_key] = dic
 
+    @staticmethod
+    def cut_useless_metadata(data: dict) -> dict:
+        processed = dict()
+        for k, v in data.items():
+            processed = v
+            break
+        return processed
 
+    async def get_current_infra_dict(self) -> dict:
+        await self._update_resource_dict()
+        return self._resources
--- server/app/main.py
+++ server/app/main.py
@@ -1,9 +1,8 @@
-from fastapi import FastAPI, Request, HTTPException
-from fastapi.responses import HTMLResponse
+import botocore.exceptions
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from .client import KloudClient
-
 from pathlib import Path
 from . import sdk_handle
 import boto3
@@ -35,24 +34,38 @@ app.add_middleware(
 class LogInReq(BaseModel):  # todo region 입력할 필요 없이 백엔드에서 모든 리전 조회 후, 인프라가 돌아가는 리전들 따로 반환
     access_key_public: str
     access_key_secret: str
-
-
-# @app.get("/", response_class=HTMLResponse)
-# async def root():
-#     return
+    region: str
 
 
 @app.post("/login")
 async def login(login_form: LogInReq):
     try:
-        kloud_client = sdk_handle.get_session_instance(access_key_id=login_form.access_key_public,
-                                                        secret_access_key=login_form.access_key_public)
-        clients[login_form.access_key_public] = kloud_client  # todo 현재 KloudClient 객체를 딕셔너리에 저장함. 추후 변동 가능
-    except "invalid_access_key":
-        raise HTTPException(status_code=404, detail="invalid_access_key")
-    return "login_success" # 로그인 성공시 Front-End에서 데이터 요청하는 것이 나아보임.
+        session_instance: boto3.Session = sdk_handle.create_session(access_key_id=login_form.access_key_public,
+                                                                    secret_access_key=login_form.access_key_secret,
+                                                                    region=login_form.region)
+        if await sdk_handle.is_valid_session(session_instance):
+            kloud_client = KloudClient(access_key_id=login_form.access_key_public,
+                                       session_instance=session_instance)
+            clients[login_form.access_key_public] = kloud_client  # todo 현재 KloudClient 객체를 딕셔너리에 저장함. 추후 변동 가능
+            return "login_success"
+    except botocore.exceptions.ClientError:
+        raise HTTPException(status_code=401, detail="login_failed")  # todo 추후 상세한 에러 내용 담아서 수정
 
 
 @app.get("/available_regions")  # 가능한 aws 지역 목록, 가장 기본적이고 보편적인 서비스인 ec2를 기본값으로 요청.
 async def get_available_regions():
     return await sdk_handle.get_available_regions()
+
+
+class InfraInfoReq(BaseModel):  # 보안 확인
+    id: str
+
+
+@app.post("/infra_info")
+async def infra_info(req: InfraInfoReq):
+    try:
+        client: KloudClient = clients[req.id]
+    except KeyError:
+        raise HTTPException(status_code=404, detail="kloud_client_not_found")
+    return await client.get_current_infra_dict()
+
