65d62a4bf410e255e8ae4fb4eb5b4fdfaf99b926
Seonghyeon Kim <kshshkim@gmail.com>
2022-03-26 14:23:46 +0900

    jwt 토큰 기반 인증

---
+++ server/app/auth.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
+from typing import Optional
+from fastapi import HTTPException, status
+from .config.token_conf import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+
+
+credentials_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials",
+)
+
+
+async def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_user_id(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("user_id")
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    return user_id
--- server/app/main.py
+++ server/app/main.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 from .client import KloudClient
 from pathlib import Path
 from . import sdk_handle
+from .auth import create_access_token, get_user_id
 import boto3
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -12,10 +13,10 @@ BASE_DIR = Path(__file__).resolve().parent
 app = FastAPI()
 aws_info = boto3.Session()
 
+clients = dict()  # 수정 필요
+
 ##### CORS #####
 # 개발 편의를 위해 모든 origin 허용. 배포시 수정 필요
-
-clients = dict()
 
 origins = [
     "*"
@@ -31,14 +32,14 @@ app.add_middleware(
 
 ##### CORS #####
 
-class LogInReq(BaseModel):  # todo region 입력할 필요 없이 백엔드에서 모든 리전 조회 후, 인프라가 돌아가는 리전들 따로 반환
+class KloudLoginForm(BaseModel):
     access_key_public: str
     access_key_secret: str
     region: str
 
 
 @app.post("/login")
-async def login(login_form: LogInReq):
+async def login(login_form: KloudLoginForm):  # todo token revoke 목록 확인, refresh token
     try:
         session_instance: boto3.Session = sdk_handle.create_session(access_key_id=login_form.access_key_public,
                                                                     secret_access_key=login_form.access_key_secret,
@@ -47,7 +48,9 @@ async def login(login_form: LogInReq):
             kloud_client = KloudClient(access_key_id=login_form.access_key_public,
                                        session_instance=session_instance)
             clients[login_form.access_key_public] = kloud_client  # todo 현재 KloudClient 객체를 딕셔너리에 저장함. 추후 변동 가능
-            return "login_success"
+            token = await create_access_token(data={"user_id": login_form.access_key_public})
+            return {"access_token": token}
+
     except botocore.exceptions.ClientError:
         raise HTTPException(status_code=401, detail="login_failed")
     except botocore.exceptions.InvalidRegionError:
@@ -60,40 +63,42 @@ async def get_available_regions():
 
 
 class InfraInfoReq(BaseModel):  # 보안 확인 필요
-    id: str
+    access_token: str
 
 
 @app.post("/infra_info")
 async def infra_info(req: InfraInfoReq):
+    client_id = await get_user_id(req.access_token)
     try:
-        client: KloudClient = clients[req.id]
+        client: KloudClient = clients[client_id]
     except KeyError:
         raise HTTPException(status_code=404, detail="kloud_client_not_found")
     return await client.get_current_infra_dict()
 
 
-class ResourceInfoReq(BaseModel):
-    id: str
-    resource_id: str
+# class ResourceInfoReq(BaseModel):
+#     id: str
+#     resource_id: str
 
 
-@app.post("/infra_specific_info")
-async def resource_info(req: ResourceInfoReq):
-    try:
-        client: KloudClient = clients[req.id]
-    except KeyError:
-        raise HTTPException(status_code=404, detail="kloud_client_not_found")
-    return await client.get_resource_info(resource_id=req.resource_id)
+# @app.post("/infra_specific_info")
+# async def resource_info(req: ResourceInfoReq):
+#     try:
+#         client: KloudClient = clients[req.id]
+#     except KeyError:
+#         raise HTTPException(status_code=404, detail="kloud_client_not_found")
+#     return await client.get_resource_info(resource_id=req.resource_id)
 
 
-class LogOutReq(BaseModel):
-    id: str
+class KloudLogoutForm(BaseModel):
+    access_token: str
 
 
 @app.post("/logout")
-async def logout(logout_form: LogOutReq):
+async def logout(logout_form: KloudLogoutForm):  # todo token revoke 목록
+    user_id = await get_user_id(logout_form.access_token)
     try:
-        clients.pop(logout_form.id)
+        clients.pop(user_id)
     except KeyError:
         pass
     finally:
