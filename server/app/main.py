2892ec0c652d3663b10e9f81c503cbd110ffc021
Seonghyeon Kim <kshshkim@gmail.com>
2022-04-26 19:01:14 +0900

    비용 내역 조회 캐시처리

--- server/app/main.py
+++ server/app/main.py
@@ -10,7 +10,7 @@ import boto3
 import asyncio
 import concurrent.futures
 from .config.cellery_app import da_app
-from .redis_req import set_cred_to_redis, get_cred_from_redis, delete_cred_from_redis
+from .redis_req import set_cred_to_redis, get_cred_from_redis, delete_cred_from_redis, get_cost_cache, set_cost_cache, delete_cache_from_redis
 
 app = FastAPI()
 aws_info = boto3.Session()
@@ -78,6 +78,15 @@ BREAK_LOOP_STATE = {'SUCCESS', 'REVOKED', 'FAILURE'}
 
 
 async def wait_until_done(celery_task: da_app.AsyncResult, interval=0.3, timeout=10.0):
+    """
+    바람직한 방법은 아닌 것 같으나, 일단 작동은 합니다.
+    asyncio.sleep(interval)로, interval(초) 만큼 이벤트 루프의 제어권을 넘깁니다.
+
+    :param celery_task:  send_task 등으로 반환된 AsyncResult 객체
+    :param interval:  결과 확인 빈도, 초 단위
+    :param timeout:  초 단위
+    :return:
+    """
     time_passed = 0
     while celery_task.state not in BREAK_LOOP_STATE:
         await asyncio.sleep(interval)
@@ -90,7 +99,7 @@ async def wait_until_done(celery_task: da_app.AsyncResult, interval=0.3, timeout
 
 
 @app.post("/login", response_model=AccessTokenResponse)
-async def login(login_form: KloudLoginForm):  # todo token revoke 목록 확인, refresh token
+async def login(login_form: KloudLoginForm):
     try:
         session_instance: boto3.Session = boto3.Session(aws_access_key_id=login_form.access_key_public,
                                                         aws_secret_access_key=login_form.access_key_secret,
@@ -119,8 +128,18 @@ async def infra_info(user_client=Depends(get_user_client)):
 
 
 @app.get("/cost/history/default")
-async def cost_history_default(user_client=Depends(get_user_client)):
-    return await user_client.get_default_cost_history()
+async def cost_history_default(user_id=Depends(get_user_id)):
+    """
+    만약 cost history 관련 수정이 이루어진 경우에도 redis 캐시는 남아있음. 에러 피하기 위해선 캐시 처리 필요.
+    """
+    cost_history = await get_cost_cache(user_id)  # 캐시 확인
+    if cost_history is None:  # 캐시가 없음.
+        user_client = await get_user_client(user_id)  # 캐시가 없을 때만 클라이언트 객체 생성.
+        cost_history: dict = await user_client.get_default_cost_history()  # aws 에서 데이터 받아옴.
+        await set_cost_cache(user_client.id, cost_history)  # 비동기 캐시
+    else:
+        print(f'cache hit {user_id=}')
+    return cost_history
 
 
 @app.get("/infra/tree")
@@ -132,6 +151,7 @@ async def infra_tree(user_client=Depends(get_user_client)):
 async def logout(user_id=Depends(get_user_id), token=Depends(security)):
     asyncio.create_task(revoke_token(token.credentials))  # 서버에서 발급한 jwt 무효화
     asyncio.create_task(delete_cred_from_redis(user_id))  # 서버에 저장된 aws sts 토큰 삭제
+    asyncio.create_task(delete_cache_from_redis(user_id))  # 캐시 제거
     return 'logout success'
 
 
@@ -149,4 +169,4 @@ async def pattern_finder2(user_client=Depends(get_user_id), token=Depends(securi
 
 @app.get("/res/{job_id}")
 async def check(job_id):
-    return da_app.AsyncResult(job_id).get()
+    return da_app.AsyncResult(job_id).get()  # 예시로, blocking io를 발생시킴.
