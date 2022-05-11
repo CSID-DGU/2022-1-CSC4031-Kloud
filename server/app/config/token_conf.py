1f173d48d511a5d88a453df5383736f3c3dab797
Kim Seonghyeon <kshshkim@gmail.com>
2022-05-11 13:02:55 +0900

    환경변수에서 secret key 가져오도록 수정

--- server/app/config/token_conf.py
+++ server/app/config/token_conf.py
@@ -1,3 +1,9 @@
-SECRET_KEY = "wow_very_secret"  # test용 샘플. 배포시 수정 필요
+import os
+
+SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+
+if SECRET_KEY is None:
+    SECRET_KEY = "wow_very_secret"  # test용 샘플. 배포시 수정 필요
+
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
