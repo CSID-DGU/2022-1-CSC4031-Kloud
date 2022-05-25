647a7edbd61812dc5f5572eef8644bf70f5ff781
hanum <rntls123@naver.com>
2022-05-25 15:09:43 +0900

    Analysis Modal

--- client/src/components/AnalysisModal.tsx
+++ client/src/components/AnalysisModal.tsx
@@ -1,3 +1,8 @@
+import PolarChart from "./AnalChartPolar";
+import LineChart from "./AnalChartLine";
+import DonutChart from "./AnalChartDonut";
+import BarChart from "./AnalChartBar";
+
 const AnalysisModal = () => {
   return <div></div>;
 };
