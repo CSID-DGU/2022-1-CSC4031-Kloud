19a1a039fa50caf2a57cdf9372f1f9fb7ef81116
hanum <rntls123@naver.com>
2022-06-02 01:48:14 +0900

    Analysis Info

--- client/src/screens/Analysis.tsx
+++ client/src/screens/Analysis.tsx
@@ -29,6 +29,19 @@ const ChartBox = styled.div<{ margin?: number }>`
     cursor: pointer;
   }
 `;
+const InfoBox = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 30px 0;
+`;
+const Info = styled.span`
+  color: white;
+  font-weight: lighter;
+  font-size: 30px;
+`;
 
 const Analysis = () => {
   const [openModal, setOpenModal] = useState<boolean>(false);
@@ -37,6 +50,12 @@ const Analysis = () => {
 
   return (
     <Container>
+      <InfoBox>
+        <Info>
+          비용 분석 페이지입니다. 차트를 클릭하여 세부 내용 및 정보를
+          확인합니다.
+        </Info>
+      </InfoBox>
       <ChartBoxContainer>
         <ChartBox
           margin={130}
@@ -60,7 +79,7 @@ const Analysis = () => {
       </ChartBoxContainer>
       <ChartBoxContainer>
         <ChartBox
-          margin={180}
+          margin={130}
           onClick={() => {
             setOpenModal((prev) => !prev);
             setSelectedChart(<LineChart modal={true} size={450}></LineChart>);
