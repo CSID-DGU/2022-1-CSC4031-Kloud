31af0b547d17ea1e41ef8f5c27c63f4a4687a849
Seonghyeon Kim <kshshkim@gmail.com>
2022-06-04 09:08:40 +0900

    refactored

--- server/app/main.py
+++ server/app/main.py
@@ -201,11 +201,12 @@ class InstanceStop(BaseModel):
 
 @app.post("/mod/instance/stop")
 async def stop_instance(req_body: InstanceStop, user_client: KloudClient = Depends(get_user_client)):
-    await asyncio.to_thread(user_client.stop_instance,
-                            instance_id=req_body.instance_id,
-                            hibernate=req_body.hibernate,
-                            force=req_body.force)
-    return 'request sent'  # todo 요청 결과 반환
+    coro = asyncio.to_thread(user_client.stop_instance,
+                             instance_id=req_body.instance_id,
+                             hibernate=req_body.hibernate,
+                             force=req_body.force)
+    asyncio.create_task(coro)
+    return 'request sent'
 
 
 class InstanceStart(BaseModel):
@@ -214,8 +215,8 @@ class InstanceStart(BaseModel):
 
 @app.post("/mod/instance/start")
 async def start_instance(req_body: InstanceStart, user_client: KloudClient = Depends(get_user_client)):
-    await asyncio.to_thread(user_client.start_instance, instance_id=req_body.instance_id)
-
+    coro = asyncio.to_thread(user_client.start_instance, instance_id=req_body.instance_id)
+    asyncio.create_task(coro)
     return 'request sent'
 
 
