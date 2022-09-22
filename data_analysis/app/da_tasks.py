aba159138389eb5430978600d59e545c68fe5b82
Seonghyeon Kim <kshshkim@users.noreply.github.com>
2022-09-22 14:44:08 +0900

    fixed /trend/similarity

--- data_analysis/app/da_tasks.py
+++ data_analysis/app/da_tasks.py
@@ -1,9 +1,9 @@
-from copyreg import pickle
+from datetime import timedelta, datetime
+
 from .da_worker import celery_task
 from .models.PatternFinder import PatternFinder
 from .models.ProPhetPatternFinder import ProPhetPatternFinder
 from .request import get_cost_info
-from datetime import timedelta, datetime
 
 
 @celery_task.task(name='add')
@@ -12,11 +12,13 @@ def add(x, y):  # 예시
 
 
 @celery_task.task(name="/cost/trend/similarity")
-def pattern_finder(token: str, start_date="2022-02-02", end_date="2022-05-10" , threshold=0.5):
+def pattern_finder(token: str, threshold=0.5):
     data = get_cost_info(token)
     p = PatternFinder(data)
     # 날짜는 수정이 가능함 원하는 날짜가 들어오게 만들면 될 듯
-    result = p.search('2022-02-02', "2022-05-17", threshold=0.5)
+    start_date = data["ResultsByTime"][0]["TimePeriod"]["Start"]
+    end_date = data["ResultsByTime"][-1]["TimePeriod"]["End"]
+    result = p.search(start_date, end_date, threshold=threshold)
     # 패턴을 못찾은 경우 추후에 try,except로 수정해야할듯
     if len(result) == 0:
         print("threshold 혹은 date범위를 바꿔주어야함")
@@ -39,17 +41,17 @@ def pattern_finder(token: str, start_date="2022-02-02", end_date="2022-05-10" , 
 
 
 @celery_task.task(name="/cost/trend/prophet")
-def pattern_finder2(token: str, yearly_seasonality, weekly_seasonality , daily_seasonality, n_changepoints, period):
+def pattern_finder2(token: str, yearly_seasonality, weekly_seasonality, daily_seasonality, n_changepoints, period):
     data = get_cost_info(token)
     answers = {}
-    periods = [5,14,30]
-    for idx,period_num in enumerate(periods):
+    periods = [5, 14, 30]
+    for idx, period_num in enumerate(periods):
         p = ProPhetPatternFinder(data=data,
-                                yearly_seasonality = yearly_seasonality,
-                                weekly_seasonality = weekly_seasonality,
-                                daily_seasonality = daily_seasonality,
-                                n_changepoints = n_changepoints,
-                                period = period_num)
+                                 yearly_seasonality=yearly_seasonality,
+                                 weekly_seasonality=weekly_seasonality,
+                                 daily_seasonality=daily_seasonality,
+                                 n_changepoints=n_changepoints,
+                                 period=period_num)
 
         p.model_fit()
         expected_data = p.expected_data()
@@ -61,12 +63,12 @@ def pattern_finder2(token: str, yearly_seasonality, weekly_seasonality , daily_s
             if i < len(real_data):
                 answer[date] = {"real_data": round(real_data.y.iloc[i], 6),
                                 "expected_data": {"yhat": round(expected_data.yhat.iloc[i], 6),
-                                                "yhat_lower": round(expected_data.yhat_lower.iloc[i], 6),
-                                                "yhat_upper": round(expected_data.yhat_upper.iloc[i], 6)}}
+                                                  "yhat_lower": round(expected_data.yhat_lower.iloc[i], 6),
+                                                  "yhat_upper": round(expected_data.yhat_upper.iloc[i], 6)}}
             else:
                 answer[date] = {"expected_data": {"yhat": round(expected_data.yhat.iloc[i], 6),
-                                                "yhat_lower": round(expected_data.yhat_lower.iloc[i], 6),
-                                                "yhat_upper": round(expected_data.yhat_upper.iloc[i], 6)}}
+                                                  "yhat_lower": round(expected_data.yhat_lower.iloc[i], 6),
+                                                  "yhat_upper": round(expected_data.yhat_upper.iloc[i], 6)}}
         if idx == 0:
             answers["day"] = answer
         elif idx == 1:
